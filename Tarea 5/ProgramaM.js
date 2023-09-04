65b825aee82e09fa035e31810b6219bf83bfd9c7
Zaira <92006681+ZairaCR@users.noreply.github.com>
2023-09-03 19:17:42 -0600

    Add Tarea 5

---
+++ Tarea 5/ProgramaM.js
@@ -0,0 +1,25 @@
+
+        const arreglo =  [8,4,7,2,9];
+
+        let m = arreglo[0];
+
+               for (let i = 1; i < arreglo.length; i++) {
+            if (arreglo[i] < m) {
+                m = arreglo[i];
+            }
+        }
+
+        // Imprimimos el resultado
+        console.log("El numero menor en el arreglo es: " + m);
+
+
+
+
+
+
+
+
+
+
+
+
